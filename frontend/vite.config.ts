c34432a961bee3e20155e8644034b9d32cb968c9
thekra-alh <202222146@yu.sa.com>
2026-06-12 04:00:49 +0300

    fix: update API URL

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,10 @@ export default defineConfig({
   },
   server: {
     proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      },
       "/onecompiler": {
         target: "https://api.onecompiler.com",
         changeOrigin: true,
